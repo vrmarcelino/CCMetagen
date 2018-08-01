0da87ab35092ea3a27f4496c6423045a34566324
vrmarcelino <vrmarcelino@gmail.com>
2018-08-01 12:48:17 +1000

    add taxnomomic info to kma results

--- tools/KMetagen.py
+++ tools/KMetagen.py
@@ -12,8 +12,17 @@ Created on Wed Jul 25 17:13:10 2018
 
 # imports
 import sys
+import csv
+import pandas as pd
 from argparse import ArgumentParser
-import fRunKMA
+import os
+from os import listdir
+import re
+
+# local imports
+#import cTaxInfo
+#import fNCBItax
+import fParseKMA
 
 # help
 if len(sys.argv) == 1:
@@ -28,13 +37,68 @@ if len(sys.argv) == 1:
 
 parser = ArgumentParser()
 
+# change this to Requires = True!
+parser.add_argument('-m', '--mode', help="""what do you want KMetagen to do? 
+                    Valid options at the moment are: parse_kma""", required=False)
 
-th = '3'
-out_fp = "06_TaxAssign/KMetagen"
-db_its="../large_databases/KMA/Unite_ITS/ITS_unite"
+parser.add_argument('-i', '--res_fp', help='Path to the KMA result (.res file)', required=False)
+parser.add_argument('-o', '--output_fp', default = 'KMetagen_out', 
+                    help='Path to the output file. Default = KMetagen_out', required=False)
+parser.add_argument('-r', '--reference_database', default = 'ITS', 
+                    help='Which reference datbase was used. Default = ITS', required=False)
 
-inR1="test_mtt_R1.fasta"
-inR2="test_mtt_R2.fasta"
+
+# what to do:
+args = parser.parse_args()
+#mode = args.mode
+
+
+# debugging:
+#out_fp = "06_TaxAssign/KMetagen"
+#in_kma_folder_fp = "KMA_long_ctgs"
+#ref_database = "ITS"
+
+mode = 'parse_kma'
+
+##### Take as input individual files and output a file with tax info
+if mode == 'parse_kma':
+    args = parser.parse_args()
+    f = args.res_fp
+    ref_database = args.reference_database
+    
+    df = pd.read_csv(f, sep='\t', index_col=0)
+
+    # Rename headers:
+    df.index.name = "Closest_match"
+ 
+    # first quality filter:
+    df = fParseKMA.res_filter(df, 20, 50, 0.2, 0.05)
+    
+    # add tax info
+    df = fParseKMA.populate_w_tax(df, ref_database)
+           
+    # save to file
+    out = args.output_fp + ".csv"
+    pd.DataFrame.to_csv(df, out)
+    
+
+
+#### To do:
+### Make output usable by KRONA!
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ tools/cTaxInfo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Classes that store taxonomic info from matches and NCBI lineage
+
+Required to parse the results of matches and store them in the SQLite3 'bench.db'
+
+@ V.R.Marcelino
+Created on 12 Jul 2018
+Last update on 13 Jul 2018
+
+"""
+
+### Stores taxid and other info
+class TaxInfo(): 
+    
+    def __init__(self, TaxId=None, Lineage=None, Sample=None, RefDatabase=None, 
+                 Abundance=None, Kingdom=None, Kingdom_TaxId=None, Phylum=None, 
+                 Phylum_TaxId=None, Class=None, Class_TaxId=None,
+                 Order=None, Order_TaxId=None, Family=None, Family_TaxId=None,
+                 Genus=None, Genus_TaxId=None, Species=None, Species_TaxId=None,
+                 Coverage=None):
+        
+        # info from matches
+        self.TaxId = TaxId
+        self.Lineage = Lineage
+        self.Sample = Sample
+        self.RefDatabase = RefDatabase
+        self.Abundance = Abundance
+
+        # info from NCBI
+        self.Kingdom = Kingdom
+        self.Kingdom_TaxId = Kingdom_TaxId
+        self.Phylum = Phylum
+        self.Phylum_TaxId = Phylum_TaxId
+        self.Class = Class
+        self.Class_TaxId = Class_TaxId
+        self.Order = Order
+        self.Order_TaxId = Order_TaxId
+        self.Family = Order
+        self.Family_TaxId = Order_TaxId
+        self.Genus = Genus
+        self.Genus_TaxId = Genus_TaxId
+        self.Species = Species
+        self.Species_TaxId = Species_TaxId
+
+        # info from Fungi Table only
+        self.Coverage = Coverage
+
+
