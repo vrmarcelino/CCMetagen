cea2356d58aa0edd6f594919efed0395abe507f4
vrmarcelino <vrmarcelino@gmail.com>
2019-01-03 15:19:11 +1100

    calc_time script completed

--- BenchmarkingTools/time_GNU/calc_time.py
+++ BenchmarkingTools/time_GNU/calc_time.py
@@ -15,50 +15,65 @@ import pandas as pd
 
 # load files
 kraken_o = "s06.6_kraken.sh.e2637431.txt"
+centrifuge_o = "s06.2_centrifuge_all.txt"
+krakenUniq_o = "s06.5_krakenUniq_test.txt"
+kma_o = "s06.1_KMA_test.txt"
+kmetagen_o = "s06.1_KMetagen_test.txt"
 
-samples = ["1_mtt_fp","2_mtg_fp","3_mtt_fp","4_nanop_fp","5_mtt_ctg_fp","6_mtg_ctg_fp","7_mtt_ctg_fp",
-           "8_mtt_fp","9_mtt_ctg_fp",
-           "1_mtt_bf","2_mtg_bf","3_mtt_bf","4_nanop_bf","5_mtt_ctg_bf","6_mtg_ctg_bf","7_mtt_ctg_bf",
-           "8_mtt_bf","9_mtt_ctg_bf",
-           "1_mtt_nt","2_mtg_nt","3_mtt_nt","4_nanop_nt","5_mtt_ctg_nt","6_mtg_ctg_nt","7_mtt_ctg_nt",
-           "8_mtt_nt","9_mtt_ctg_nt"]
 
-ref_databases = ["RefSeq fungi partial", "RefSeq bf", "nt"] 
+# save as
+output = "results.csv"
 
-# dict to store results:
-results_dict = {}
+
+def calc_CPU_time(u_line,s_line):
+    split = re.split(r'(\t|\n|m|s)', u_line)
+    
+    # convert secs to mins
+    um1=float(split[6]) / 60
+    #total user time in min:
+    um = float(split[4]) + um1
+            
+    ### get sys time (min)
+    split = re.split(r'(\t|\n|m|s)', s_line)
+    # convert secs to mins
+    sm1=float(split[8]) / 60
+    #total user time in min:
+    sm = float(split[6]) + sm1
+            
+    CPU_time = um + sm
+    CPU_time = round(CPU_time, 4)
+    return (CPU_time)
+
 
 
 # create new dataframe:
-time_results = pd.DataFrame()
+time_results = pd.DataFrame(columns=['Sample', 'CPU_time', 'Ref_DB', 'Program'])
 
+
+samples = ["1_mtt","2_mtg","3_mtt","4_nanop","5_mtt_ctg","6_mtg_ctg","7_mtt_ctg",
+           "8_mtt","9_mtt_ctg",
+           "1_mtt","2_mtg","3_mtt","4_nanop","5_mtt_ctg","6_mtg_ctg","7_mtt_ctg",
+           "8_mtt","9_mtt_ctg",
+           "1_mtt","2_mtg","3_mtt","4_nanop","5_mtt_ctg","6_mtg_ctg","7_mtt_ctg",
+           "8_mtt","9_mtt_ctg"]
+
+
+
+### Kraken2:
 counter = 0
-
 program = "kraken"
-output = "kraken_results"
+ref_databases = ["RefSeq_f_partial", "RefSeq_bf", "nt"]
 
 with open(kraken_o) as o:
     for line in o:
         if line.startswith("user"):
             
             ### get user time (min)
-            split = re.split(r'(\t|\n|m|s)', line)
-            # convert secs to mins
-            um1=float(split[6]) * 0.0166667
-            #total user time in min:
-            um = float(split[4]) + um1
+            user_line = line
+            sys_line = next(o)
             
-            ### get sys time (min)
-            sys_str = next(o)
-            split = re.split(r'(\t|\n|m|s)', sys_str)
-            # convert secs to mins
-            sm1=float(split[8]) * 0.0166667
-            #total user time in min:
-            sm = float(split[6]) + sm1
-            
-            CPU_time = um + sm
-            CPU_time = round(CPU_time, 4)
-            
+            CPU_time = calc_CPU_time(user_line,sys_line)
+                       
             if 0 <= counter < 9:
                 ref = ref_databases[0]
                 
@@ -70,18 +85,149 @@ with open(kraken_o) as o:
             else:
                 print ("number of results don't match")
                 
-            results = [program, ref, CPU_time]
             sample = samples[counter]
-            results_dict[sample] = results
+            results = pd.Series([sample, CPU_time, ref, program], index=['Sample', 'CPU_time', 'Ref_DB', 'Program'])
+            time_results = time_results.append(results,ignore_index=True)
             
             counter += 1
+
+
+
+### Centrifuge:
+counter = 0
+program = "Centrifuge"
+ref_databases = ["nt","RefSeq_bf", "RefSeq_f_partial"]
+
+
+with open(centrifuge_o) as o:
+    for line in o:
+        if line.startswith("user"):
             
-df = pd.DataFrame.from_dict(results_dict,orient='index')
-out = output + ".csv"
-pd.DataFrame.to_csv(df, out)
+            ### get user time (min)
+            user_line = line
+            sys_line = next(o)
+            
+            CPU_time = calc_CPU_time(user_line,sys_line)
+                       
+            if 0 <= counter < 9:
+                ref = ref_databases[0]
+                
+            elif 9 <= counter < 18:
+                ref = ref_databases[1]
+            
+            elif 18 <= counter < 27:
+                ref = ref_databases[2]
+            else:
+                print ("number of results don't match")
+                
+            sample = samples[counter]
+            results = pd.Series([sample, CPU_time, ref, program], index=['Sample', 'CPU_time', 'Ref_DB', 'Program'])
+            time_results = time_results.append(results,ignore_index=True)
+            
+            counter += 1
 
 
 
+
+### KrakenUniq (need the result of one more sample)
+counter = 0
+program = "KrakenUniq"
+ref_databases = ["RefSeq_f_partial","RefSeq_bf"]
+
+
+with open(krakenUniq_o) as o:
+    for line in o:
+        if line.startswith("user"):
+            
+            ### get user time (min)
+            user_line = line
+            sys_line = next(o)
+            
+            CPU_time = calc_CPU_time(user_line,sys_line)
+                       
+            if 0 <= counter < 9:
+                ref = ref_databases[0]
+                
+            elif 9 <= counter < 18:
+                ref = ref_databases[1]
+
+            else:
+                print ("number of results don't match")
+                
+            sample = samples[counter]
+            results = pd.Series([sample, CPU_time, ref, program], index=['Sample', 'CPU_time', 'Ref_DB', 'Program'])
+            time_results = time_results.append(results,ignore_index=True)
+            
+            counter += 1
+
+##########################
+######## KMA and Kmetagen
+            
+store_kma = []
+store_filtering = pd.DataFrame(columns=['Sample', 'CPU_time', 'Ref_DB', 'Program'])
+
+counter = 0
+program = "KMetagen"
+ref_databases = ["UNITE","RefSeq_f_partial","RefSeq_bf","nt"]
+samples = ["2_mtg","3_mtt","4_nanop","6_mtg_ctg","7_mtt_ctg",
+           "8_mtt","9_mtt_ctg",
+           "1_mtt","2_mtg","3_mtt","4_nanop","5_mtt_ctg","6_mtg_ctg","7_mtt_ctg",
+           "8_mtt","9_mtt_ctg",
+           "1_mtt","2_mtg","3_mtt","4_nanop","5_mtt_ctg","6_mtg_ctg","7_mtt_ctg",
+           "8_mtt","9_mtt_ctg",
+           "1_mtt","2_mtg","3_mtt","4_nanop","5_mtt_ctg","6_mtg_ctg","7_mtt_ctg",
+           "8_mtt","9_mtt_ctg"]
+
+# save KMA's time to a list
+with open(kma_o) as o:
+    for line in o:
+        if line.startswith("user"):
+            
+            ### get user time (min)
+            user_line = line
+            sys_line = next(o)
+
+            CPU_time = calc_CPU_time(user_line,sys_line)
+                       
+            store_kma.append(CPU_time)
+
+# ten add it to Kmetagen's CPU time:
+with open(kmetagen_o) as o:
+    for line in o:
+        if line.startswith("user"):
+            
+            ### get user time (min)
+            user_line = line
+            sys_line = next(o)
+            
+            CPU_time_filtering = calc_CPU_time(user_line,sys_line)
+            KMA_time = store_kma[counter]
+            CPU_time = CPU_time_filtering + KMA_time
+            
+            if 0 <= counter < 9:
+                ref = ref_databases[0]
+                
+            elif 9 <= counter < 18:
+                ref = ref_databases[1]
+                
+            elif 18 <= counter < 27:
+                ref = ref_databases[2]
+                
+            elif 27 <= counter < 34:
+                ref = ref_databases[3]
+                
+            else:
+                print ("number of results don't match")
+                
+            sample = samples[counter]
+            results = pd.Series([sample, CPU_time, ref, program], index=['Sample', 'CPU_time', 'Ref_DB', 'Program'])
+            time_results = time_results.append(results,ignore_index=True)
+            
+            counter += 1
+
+
+
+pd.DataFrame.to_csv(time_results, output)
 
 
 
