4b2766628c5392ec66c39bc992786037d617a630
Vini Salazar <17276653+vinisalazar@users.noreply.git>
2023-12-19 17:55:14 +1100

    Improve renaming script
      - Edit input file names to match documentation
      - Add logging to see how long script is taking
      - Add log statements to see progress
      - Make sure script operates lazily

--- docs/benchmarking/rename_nt/rename_nt.py
+++ docs/benchmarking/rename_nt/rename_nt.py
@@ -6,45 +6,45 @@ Script to add taxids to nt collection
 @ V.R.Marcelino
 Created on Fri Dec 28 10:37:56 2018
 """
-
 import re
+import logging
 
 
-# input files:
-acc2taxid_map="accession_taxid_nucl.map"
-nt_in="nt.fa"
+logging.info("Starting script.")
 
+# Input files:
+acc2taxid_map = "accession_taxid_nucl.map"
+nt_in = "nt_sequential.fa"
 
-# function to  get taxids from accession numbers
-def get_tax_id_dic (accession, accession_dic):
-    taxid = accession_dic.get(accession)
-    if taxid == None:
-        taxid = "unk_taxid"
+# Output file:
+nt_out = "nt_w_taxid.fas"
+
+# Function to get taxids from accession numbers
+def get_tax_id_dic(accession, accession_dic):
+    taxid = accession_dic.get(accession, "unk_taxid")
     return taxid
 
-
-# read acc2taxid.map as dictionary
+# Read acc2taxid.map as a dictionary
+logging.info("Reading acc2taxid.map as a dictionary.")
 with open(acc2taxid_map) as a:
     acc2tax_dic = dict(x.rstrip().split(None, 1) for x in a)
-    
 
-# read fasta file and output new file on the fly
-# although it says 'open', it reads line by line and won't store the whole file in memory.
-    
-new_nt = open('nt_w_taxid.fas', 'w')
+logging.info(f"Dictionary has {len(acc2tax_dic)} entries.")
 
-with open(nt_in) as nt:
+# Read fasta file and write the new file on the fly
+logging.info("Reading nt file and writing new file.")
+with open(nt_in) as nt, open(nt_out, 'w') as new_nt:
+    ix = 0
     for line in nt:
+        ix += 1
+        if not ix % 1000000:
+            logging.info("Processed {} lines.".format(ix))
+
         if line.startswith(">"):
-            splited_rec = re.split (r'(>| )', line)
+            splited_rec = re.split(r'(>| )', line)
             accession = splited_rec[2]
-            taxid = get_tax_id_dic(accession,acc2tax_dic)
+            taxid = get_tax_id_dic(accession, acc2tax_dic)
             line = ">" + taxid + "|" + "".join(splited_rec[2:])
         new_nt.write(line)
-            
-new_nt.close()
 
-
-print ("Done!")
-
-
+logging.info("All done.")
