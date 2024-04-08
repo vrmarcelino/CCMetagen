vrmarcelino/CCMetagen

commit 7d39edae8fdafdbb86790d82507ad427d9420838

Use dict matching instead of loop to iterate over ranks

ccmetagen/fParseKMA.py
@@ -11,8 +11,7 @@ Created on 1 Aug 2018.
 import re
 
 # local imports
-from ccmetagen import cTaxInfo
-from ccmetagen import fNCBItax
+from ccmetagen import cTaxInfo, fNCBItax, _RANK_THRESHOLDS
 
 # function to filter a res file in pandas df format:
 def res_filter(df, ref_database, cov, Iden, Depth, p):
@@ -136,35 +135,22 @@ def populate_w_tax(
         if match_info.Kingdom == "Fungi":
             in_df.at[index, "LCA_TaxId"] = 4751
 
-        # fill in the rest of the table according to similarity threshold:
-        if qiden >= phylum_threshold:
-            in_df.at[index, "Phylum"] = match_info.Phylum
-            if match_info.Phylum_TaxId != None:
-                in_df.at[index, "LCA_TaxId"] = match_info.Phylum_TaxId
+        thresholds = {
+            "Phylum": phylum_threshold,
+            "Class": class_threshold,
+            "Order": order_threshold,
+            "Family": family_threshold,
+            "Genus": genus_threshold,
+            "Species": species_threshold
+        }
 
-        if qiden >= class_threshold:
-            in_df.at[index, "Class"] = match_info.Class
-            if match_info.Class_TaxId != None:
-                in_df.at[index, "LCA_TaxId"] = match_info.Class_TaxId
+        for rank, threshold in thresholds.items():
+            if qiden >= threshold:
+                tax_info_attr = f"{rank}_TaxId"
 
-        if qiden >= order_threshold:
-            in_df.at[index, "Order"] = match_info.Order
-            if match_info.Order_TaxId != None:
-                in_df.at[index, "LCA_TaxId"] = match_info.Order_TaxId
+                in_df.at[index, rank] = getattr(match_info, rank)
 
-        if qiden >= family_threshold:
-            in_df.at[index, "Family"] = match_info.Family
-            if match_info.Family_TaxId != None:
-                in_df.at[index, "LCA_TaxId"] = match_info.Family_TaxId
+                if getattr(match_info, tax_info_attr) is not None:
+                    in_df.at[index, "LCA_TaxId"] = getattr(match_info, tax_info_attr)
 
-        if qiden >= genus_threshold:
-            in_df.at[index, "Genus"] = match_info.Genus
-            if match_info.Genus_TaxId != None:
-                in_df.at[index, "LCA_TaxId"] = match_info.Genus_TaxId
-
-        if qiden >= species_threshold:
-            in_df.at[index, "Species"] = match_info.Species
-            if match_info.Species_TaxId != None:
-                in_df.at[index, "LCA_TaxId"] = match_info.Species_TaxId
-
-    return in_df
+        return in_df

commit 6123fbcda3902eb6c279beb0dbc51fbab03d714c

Fix problem with lineage_parser
  - Fix indentation error introduced in 7d39edae
  - Lineage parser now working as intended
  - Remove unused argument in func 'res_filter'
  - Remove unused import in fParseKMA
    - Arguments set in _RANK_THRESHOLDS are explicitly passed to func 'populate_w_tax'

CCMetagen.py
@@ -389,7 +389,7 @@ if __name__ == "__main__":
     ##### Quality control + taxonomic assignments
 
     # quality filter (coverage, query identity, Depth and p-value)
-    df = fParseKMA.res_filter(df, ref_database, c, q, d, p)
+    df = fParseKMA.res_filter(df, c, q, d, p)
 
     # add tax info
     df = fParseKMA.populate_w_tax(df, ref_database, st, gt, ft, ot, ct, pt)

ccmetagen/fParseKMA.py
@@ -11,10 +11,10 @@ Created on 1 Aug 2018.
 import re
 
 # local imports
-from ccmetagen import cTaxInfo, fNCBItax, _RANK_THRESHOLDS
+from ccmetagen import cTaxInfo, fNCBItax
 
 # function to filter a res file in pandas df format:
-def res_filter(df, ref_database, cov, Iden, Depth, p):
+def res_filter(df, cov, Iden, Depth, p):
     df = df.drop(df[df.Template_Coverage < cov].index)
 
     # filter based on identity
@@ -121,7 +121,6 @@ def populate_w_tax(
                 )
 
         # Populate the df with lineage info and the LCA taxid:
-
         in_df.at[index, "Superkingdom"] = match_info.Superkingdom
         in_df.at[index, "Kingdom"] = match_info.Kingdom
 
@@ -153,4 +152,4 @@ def populate_w_tax(
                 if getattr(match_info, tax_info_attr) is not None:
                     in_df.at[index, "LCA_TaxId"] = getattr(match_info, tax_info_attr)
 
-        return in_df
+    return in_df