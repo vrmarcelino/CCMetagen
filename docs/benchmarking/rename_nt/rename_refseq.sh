4d6dc8e09d29e735c242d20b7f529c487d73ef8d
handibles <genographer@gmail.com>
2025-04-02 15:47:21 +0100

    catch missing sort -u, x20 faster
     ^ - opportune `sort-uniq` added to `join` operation; now performs `[nfile]` operations instead of `[naccession]` operations which is <<. 
    
     ^ - clarified also log names
    
     ^ - change in `wget acc2tax` from `ftp...` to `https...`, as former was failing
    
    
    v - script remains utterly fragile..

--- docs/benchmarking/rename_nt/rename_refseq.sh
+++ docs/benchmarking/rename_nt/rename_refseq.sh
@@ -16,14 +16,15 @@ if [ -z $threeds ] ; then threeds=20 ; fi
 mkdir -p $workdir
 
 echo " + 1 +   recommended to dl some NCBI RefSeq genomes - 30m  ---------------"
-wget https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt -O $workdir/assembly_summary.txt   > $workdir/wgetlog.temp 2>&1  
+wget https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt -O $workdir/assembly_summary.txt   > $workdir/wgetlog1.temp 2>&1  
 grep "reference genome" $workdir/assembly_summary.txt >> $workdir/assembly_summary_ref.txt                                # 20,858 references
-cut -f20 $workdir/assembly_summary_ref.txt | parallel -j $threeds "wget -r -np -nH --cut-dirs=5 {}/{/}_genomic.fna.gz -O $workdir/{/}_genomic.fna.gz" >> $workdir/wgetlog.temp 2>&1  
+cut -f20 $workdir/assembly_summary_ref.txt | parallel -j $threeds "wget -r -np -nH --cut-dirs=5 {}/{/}_genomic.fna.gz -O $workdir/{/}_genomic.fna.gz" >> $workdir/wgetlog1.temp 2>&1  
 # 26GB, 29min, Mar.2025 - 20,860 fna.gz
 
+
 echo " + 2 +   get RefSeq ("_") accession-taxid listings - 10m?  ---------------"
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz -O $workdir/nucl_gb.accession2taxid.gz >> $workdir/wgetlog.temp 2>&1           # 2.3/13GB - non TranscrShotAss / WGS
-wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.gz -O $workdir/nucl_wgs.accession2taxid.gz >> $workdir/wgetlog.temp 2>&1   # 5.0/33GB - TranscrShotAss / WGS
+wget http://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz -O $workdir/nucl_gb.accession2taxid.gz >> $workdir/wgetlog2.temp 2>&1           # 2.3/13GB - non TranscrShotAss / WGS
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.gz -O $workdir/nucl_wgs.accession2taxid.gz >> $workdir/wgetlog3.temp 2>&1   # 5.0/33GB - TranscrShotAss / WGS
 zgrep "_" $workdir/nucl_*.accession2taxid.gz | cut -f 2-3 > $workdir/accession_taxid_nucl-rs.map                          # 124,253,447 entries ; says 3m but thats not true
 
 
@@ -32,18 +33,17 @@ accession_filename () {
   zcat $1 | awk -v filen=$1 '/^>/ {print substr($1, 2), "\t", filen}'
 }
 export -f accession_filename
-parallel --keep-order -j $threeds accession_filename {} ::: ${workdir}/*genomic.fna.gz > $workdir/accession_fqpath-rs.map
-# 3m, 1,074,620 entries, 20857 fna.gz 
+parallel --keep-order -j $threeds accession_filename {} ::: ${workdir}/*genomic.fna.gz > $workdir/accession_fqpath-rs.map  # 3m, 1,074,620 entries, 20857 fna.gz 
 
 
-echo " + 4 +   sort & join two lists by accession - 1m  ----(ignore 9606 error!)"
-join -j 1 -e XXXcatastrophotronXXXX -o 1.2,2.2 <( sort $workdir/accession_taxid_nucl-rs.map ) <( sort $workdir/accession_fqpath-rs.map ) | tr " " "\t" > $workdir/taxid_fqpath.map
+echo " + 4 +   sort & join two lists by accession - 3m  ----(ignore 9606 error!)"
+time join -j 1 -e XXXcatastrophotronXXXX -o 1.2,2.2 <( sort $workdir/accession_taxid_nucl-rs.map ) <( sort $workdir/accession_fqpath-rs.map ) | tr " " "\t" | sort -u > $workdir/taxid_fqpath.map
 ## 
 # join: /dev/fd/63:307894: is not sorted: NM_001000.4     9606
 # join: input is not in sorted order
 
 
-echo " + 5 +   re-stamp headers - ~200m  ----------------------------------------"
+echo " + 5 +   re-stamp headers - 30m  -----------------------------------------"
 cat $workdir/taxid_fqpath.map | \
   parallel --colsep "\t" -j $threeds "gzip -dc {2} | sed -E \"s/>/>{1}\|/g\" > {=2 s/(.*).fna.gz/\${1}_taxID2.fna/=} "  >> $workdir/stamplog.temp 2>&1
 
